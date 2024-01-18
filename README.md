781e8c90050a4b68b0bd58ddc1a6a970a514360f
JasonLei <jlei@activision.com>
2024-01-17 21:31:59 -0800

    completed demo

--- README.md
+++ README.md
@@ -2,6 +2,31 @@
 
 ## Getting Started
 
+### Step 1: Create `demo-app/`.
 ```bash
 $ npx create-react-app demo-app
+```
+
+Then drop the two initial files `routerexample1.js` and `routerexample2.js` into `demo-app/src/`.
+
+### Step 2: Install dependencies.
+```bash
+$ npm install react-router-dom
+```
+
+### Step 3: Complete the two demos.
+
+After you complete each demo, go to `demo-app/src/index.js`.
+
+Comment out the line
+```js
+import App from './App';
+```
+and for demo-1 add
+```js
+import App from './routerexample1'
+```
+and for demo-2 add
+```js
+import App from './routerexample2'
 ```
--- demo-app/src/routerexample1.js
+++ demo-app/src/routerexample1.js
@@ -1,13 +1,61 @@
 import React from "react";
 import {
-  
-} from "react-router-dom";
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom"; // 6.21.2
 
 export default function App() {
   return (
     <Router>
-      
-     
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </div>
     </Router>
   );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>This is the Home page.</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is the About page.</p>
+    </div>
+  )
+}
+
+function Users() {
+  return (
+    <div>
+      <h2>Users</h2>
+      <p>This is the Users page.</p>
+    </div>
+  )
 }
