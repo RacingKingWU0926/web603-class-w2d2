e38e2c8a9589aa95a96f612cdfbaa9ff8a393b58
JasonLei <jlei@activision.com>
2024-01-18 21:22:03 -0800

    completed demo2

--- demo-app/src/routerexample2.js
+++ demo-app/src/routerexample2.js
@@ -1,30 +1,78 @@
 import React from "react";
 import {
-  
-} from "react-router-dom";
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom"; // 6.21.2
 
 export default function App() {
   return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/topics">Topics</Link>
-            </li>
-          </ul>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics/">Topics</Link>
+          </li>
+        </ul>
 
-          <Routes>
-            <Route path="/about" element={ <About />} />
-            <Route path="/topics" element={<Topics />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={ <About />} />
+          <Route path="/topics/*" element={<Topics />} /> {/* add `/*` to indicate the existence of descendents */}
+        </Routes>
+      </div>
     </Router>
   );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>This is the Home page.</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is the About page.</p>
+    </div>
+  )
+}
+
+function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <p>This is the Topics page.</p>
+      <h3>Please select a topic.</h3>
+      <ul>
+        <li>
+          <Link to="cats">Cats</Link>
+        </li>
+        <li>
+          <Link to="dogs">Dogs</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path=":topicId" element={<Topic />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h4>Requested topic ID: {topicId}</h4>;
 }
